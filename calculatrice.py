5d2996cd780456c453d31250f68a056853ee37f1
danielzehima <danielzehia@gail.com>
2025-10-27 09:26:28 +0000

    ajouter des commentaires dans le fichier cacultrice

--- calculatrice.py
+++ calculatrice.py
@@ -2,16 +2,16 @@
 #déclaration de 02 variables
 nombre1=input("Entrez le premier nombre: ")
 nombre2=input("Entrez le deuxième nombre: ")
-
+#vérification que les entrées sont des nombres entiers
 if not nombre1.isnumeric() or  not nombre2.isnumeric():
     print("Erreur: Veuillez entrer des nombres entiers valides.")
     raise SystemExit("Fin de programme")
 
 nombre1=int(nombre1)
 nombre2=int(nombre2)
-    #affichage du menu
+#demande de l'opération à effectuer
 operation=input("Choisissez l'opération à effectuer entre ['+','-','*' ou '/' ] :")
-
+#vérification de l'opérateur
 if operation not in ['+','-','*','/']:
     print("Erreur: Opérateur invalide.")
     raise SystemExit("Fin de programme")
@@ -29,7 +29,7 @@ elif operation=="/":
         resultat=round(nombre1/nombre2,2)
 else:
     print("Opération invalide")
-print("Le résultat de l'opération est:",round(resultat,2)
+print("Le résultat de l'opération est:",round(resultat,2))
     
               
             
