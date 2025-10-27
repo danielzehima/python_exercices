ed0667c62ddd98b6675eded5e1eabf4ab1b2ed23
danielzehima <danielzehia@gail.com>
2025-10-27 09:20:27 +0000

    ajouter du fichier calculatrice

---
+++ calculatrice.py
@@ -0,0 +1,35 @@
+#création d'ne calculatrice simple en python
+#déclaration de 02 variables
+nombre1=input("Entrez le premier nombre: ")
+nombre2=input("Entrez le deuxième nombre: ")
+
+if not nombre1.isnumeric() or  not nombre2.isnumeric():
+    print("Erreur: Veuillez entrer des nombres entiers valides.")
+    raise SystemExit("Fin de programme")
+
+nombre1=int(nombre1)
+nombre2=int(nombre2)
+    #affichage du menu
+operation=input("Choisissez l'opération à effectuer entre ['+','-','*' ou '/' ] :")
+
+if operation not in ['+','-','*','/']:
+    print("Erreur: Opérateur invalide.")
+    raise SystemExit("Fin de programme")
+if operation=="+":
+    resultat=nombre1+nombre2
+elif operation=="-":
+    resultat=nombre1-nombre2 
+elif operation=="*":
+    resultat=nombre1*nombre2
+elif operation=="/":
+    if nombre2==0:
+        print("La division par zéro n'est pas autorisée.")
+        raise SystemExit("Fin de programme")
+    else:
+        resultat=round(nombre1/nombre2,2)
+else:
+    print("Opération invalide")
+print("Le résultat de l'opération est:",round(resultat,2)
+    
+              
+            
